a8a4f52f386a61b0233b105de0bac33bc142ceab
Sasa Milovic <sasa.milovic@asos.com>
2017-10-18 20:52:04 +0100

    add mutate global state without awaiting test

--- AsyncStuff/Class1.cs
+++ AsyncStuff/Class1.cs
@@ -7,6 +7,11 @@ using NUnit.Framework;
 
 namespace AsyncStuff
 {
+    public class MutableGlobalState
+    {
+        public static int State;
+    }
+    
     [TestFixture]
     public class AsyncTest
     {
@@ -15,10 +20,22 @@ namespace AsyncStuff
         {
             // what is the problem with this (only relevant with certain sync contexts)
             const string expected = "foo";
+            
             var actual = Methods
                             .WaitASecondAndReturnAValue(expected)
                             .Result;
+            
             Assert.AreEqual(expected, actual);
-        }     
+        }
+
+        [Test]
+        public void MethodReturnsATaskOfTAndMutatesGlobalState()
+        {
+            const int expected = 5;
+
+            Methods.MutateGlobalState();
+            
+            Assert.AreEqual(expected, MutableGlobalState.State);
+        }
     }
 }
--- AsyncStuff/Methods.cs
+++ AsyncStuff/Methods.cs
@@ -7,8 +7,14 @@ namespace AsyncStuff
     {
         public static Task<string> WaitASecondAndReturnAValue(string valueToReturn)
         {
-            Task.Delay(TimeSpan.FromSeconds(1));
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
             return Task.FromResult(valueToReturn);
+        }
+
+        public static Task MutateGlobalState()
+        {
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();            
+            return Task.Factory.StartNew(() => MutableGlobalState.State = 5);
         }
     }
 }
